7bc03d86598fc598eac322d47efe4331c9ee7e1c
hone1er <josephmvillavicencio@gmail.com>
2019-06-11 13:18:29 -0700

    works

--- app.py
+++ app.py
@@ -11,16 +11,19 @@ app = Flask(__name__)
 #          Flask App Routes         #
 #####################################
 
+
 @app.route("/names/<name>", methods=["GET"])
 def graph(name):
-    names = pd.read_csv("Resources/namedf.csv")[["name","gender","count","year"]]
+    names = pd.read_csv(
+        "Resources/namedf.csv")[["name", "gender", "count", "year"]]
     # Format the name
     name = name.title()
     # Make a plot for Male and Female
     jsond = {}
     print(f"this is the name: {name}")
     for gender in ['F', 'M']:
-        namedf = names[(names["name"]==f"{name}") & (names["gender"]==f"{gender}")][["year","count"]].reset_index().drop('index', axis=1)
+        namedf = names[(names["name"] == f"{name}") & (names["gender"] == f"{gender}")][[
+            "year", "count"]].reset_index().drop('index', axis=1)
         namedict = namedf.to_dict(orient='list')
         if gender == 'M':
             jsond["Male"] = namedict
@@ -30,19 +33,17 @@ def graph(name):
 #####################################
 #          Flask App Routes         #
 #####################################
-#Home Page
+# Home Page
 @app.route("/")
 def index():
     full_filename = "static/img/graph.png"
-    return render_template("index.html", maleimg = full_filename, femaleimg = full_filename, name='Json')
+    return render_template("index.html", maleimg=full_filename, femaleimg=full_filename, name='Json')
 
 
 # About Page
 @app.route("/about.html")
 def about():
     return render_template("about.html")
-   
-    
 
 
 if __name__ == "__main__":
