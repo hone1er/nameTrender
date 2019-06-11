9a301dd74d4f714cafa8f8b60fdd75548590b8a8
hone1er <josephmvillavicencio@gmail.com>
2019-06-11 13:36:18 -0700

    init

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, Response, jsonify
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
-from namesdictionary import pregraphedMales, pregraphedFemales
 
 
 app = Flask(__name__)
